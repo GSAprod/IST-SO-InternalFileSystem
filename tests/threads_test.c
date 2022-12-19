9c4a29b30ab91ae112095304492cf20a1b6d00bb
GSA prod <goncalo.santana.alves@gmail.com>
2022-12-19 23:11:44 +0000

    Some fixes on tests

--- tests/threads_test.c
+++ tests/threads_test.c
@@ -61,9 +61,9 @@ void assert_contents_ok(char const *path) {
     assert(f != -1);
 
     uint8_t buffer[sizeof(file_contents)];
-    tfs_read(f, buffer, sizeof(buffer));
+    ssize_t r = tfs_read(f, buffer, sizeof(buffer));
     printf("aqui - %s\n",buffer);
-    assert(tfs_read(f, buffer, sizeof(buffer)) == sizeof(buffer));
+    assert(r == sizeof(buffer));
     assert(memcmp(buffer, file_contents, sizeof(buffer)) == 0);
 
     assert(tfs_close(f) != -1);
--- tests/threads_test_write_to_two_files.c
+++ tests/threads_test_write_to_two_files.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <errno.h>
+#include <assert.h>
 
 #define APPEND_CONTENTS_FILE ("123")
 #define APPEND_CONTENTS_SYMLINK ("ABC")
@@ -17,16 +19,19 @@ typedef struct {
     char *text;
 } write_args_t;
 
+uint8_t const file_1_contents[] = "789789789789789789789789789789";
+
 void *write_to_path_fn(void* arg) {
     write_args_t const *args = (write_args_t const *)arg;
 
     size_t total_writes = 0;
     for(size_t i = 0; i < args->n_writes; i++) {
         int f = tfs_open(args->path, TFS_O_APPEND);
+        assert(f != -1);
 
-        tfs_write(f, args->text, strlen(args->text));
+        assert(tfs_write(f, args->text, strlen(args->text)) == strlen(args->text));
 
-        tfs_close(f);
+        assert(tfs_close(f) != -1);
 
         total_writes++;
 
@@ -53,6 +58,19 @@ void print_file_contents(char *path) {
     tfs_close(fd);
 }
 
+void assert_contents_ok(char const *path) {
+    int f = tfs_open(path, 0);
+    assert(f != -1);
+
+    uint8_t buffer[sizeof(file_1_contents)];
+    ssize_t r = tfs_read(f, buffer, sizeof(buffer));
+    printf("aqui - %s\n",buffer);
+    assert(r == sizeof(buffer));
+    assert(memcmp(buffer, file_1_contents, sizeof(buffer)) == 0);
+
+    assert(tfs_close(f) != -1);
+}
+
 int main() {
     char *file_path = "/f1";
     char *file_path_2 = "/f2";
@@ -65,9 +83,11 @@ int main() {
 
     // Create file with content
     int f1 = tfs_open(file_path, TFS_O_CREAT);
+    assert(f1 != -1);
     int f2 = tfs_open(file_path_2, TFS_O_CREAT);
-    tfs_close(f1);
-    tfs_close(f2);
+    assert(f2 != -1);
+    assert(tfs_close(f1) != -1);
+    assert(tfs_close(f2) != -1);
 
     // Create all arguments for the threads
     write_args_t thread_args_1 = { .num = 1, .n_writes = total_ops, .path = file_path, .text = "789"};
@@ -91,6 +111,9 @@ int main() {
     printf("FINISH!!!\n");
     print_file_contents(file_path);
     print_file_contents(file_path_2);
+
+    assert_contents_ok(file_path);
+    
     tfs_destroy();
 
     return 0;
